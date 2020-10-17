39c3eec6aeb00220cf9975a7c9d29b6763278ed9
trainer-coder <audreetrainees@gmail.com>
2020-10-17 17:15:53 +0530

    changes-6

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 passport.use(
   new GoogleStrategy(
     {
-      callbackURL: "http://0.0.0.0:5000/auth/google/callback",
+      callbackURL: "http://localhost:5000/auth/google/callback",
       clientID:
         "448989001757-1jur341vs47fp5hvhnmfp56gljk7oi8i.apps.googleusercontent.com",
       clientSecret: "VRZDMArVNYXlXu9ynMyvWX7z",
