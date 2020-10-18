2a2654daaf257bd8c91f0885aceb44fa6444c138
trainer-coder <audreetrainees@gmail.com>
2020-10-18 15:34:33 +0530

    changes -7

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 });
 
 const MONGODB_URI = "mongodb+srv://tasker-coder:taskeratlas4@mycluster.cb8cm.mongodb.net/<dbname>?retryWrites=true&w=majority"
-mongoose.connect(MONGODB_URI ||"mongodb://localhost:27017/Icrowdlogin", {
+mongoose.connect(MONGODB_URI , {
   useNewUrlParser: true,
 });
 
